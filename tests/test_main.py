9f78cfd84c039fa2c0de673ad05352f578a6997b
harrydowning <harry.downing17@gmail.com>
2023-04-04 12:46:31 +0100

    test for user command substitution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys, os, subprocess
+from types import SimpleNamespace
 import pytest
 import serl.main as main
 from test_utils import tokens, token_split, exp_tokens
@@ -20,6 +21,40 @@ def test_extension(file, platform, expected):
     actual = main.extension(file)
     assert actual == expected
     os.name = tmp
+
+def test_exec_and_eval():
+    pass
+
+env = {
+    'dict': {
+        'key': 'v1'
+    },
+    'list': ['v2', 'v3', 'v4'],
+    'var': 'v5',
+    'f': lambda *args, **kwargs: 'v6',
+    'tf': main.Traversable(lambda *args, **kwargs: 'v7')
+}
+
+@pytest.mark.parametrize('command, expected', [
+    ('cmd ${{HOME}}', 'cmd ${HOME}'),
+    ('cmd {dict[key]}', 'cmd v1'),
+    ('cmd {list[1]}', 'cmd v3'),
+    ('cmd {var}', 'cmd v5'),
+    ('cmd {f}', f'cmd {env["f"]}'),
+    ('cmd {tf}', f'cmd v7'),
+])
+def test_run_command(command, expected):
+    tmp_run = subprocess.run
+    
+    def correct_call(command, *args, **kwargs):
+        assert command == expected
+        return SimpleNamespace(stdout=None)
+    subprocess.run = correct_call
+    main.run_command(command, env)
+    subprocess.run = tmp_run
+
+def test_get_execute_func():
+    pass
 
 @pytest.mark.parametrize('argv,  base_args_expected, args_expected', [
     (['link/for/lang1'], {'<command>': 'run'}, {'<language>': 'lang1'}),
